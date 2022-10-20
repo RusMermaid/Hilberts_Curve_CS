19e6a298459a0234d7c0522ea7727a9508f5bc84
RusGol718 <ruslanf743@gmail.com>
2022-10-20 17:15:29 +0300

    V3.0

--- HilbertsCurve_Python/python3.9/collections/abc.py
+++
@@ -1,5 +0,0 @@
-from _collections_abc import *
-from _collections_abc import __all__
-from _collections_abc import _CallableGenericAlias
-from _collections_abc import __name_for_get_source__, __loader__
-
